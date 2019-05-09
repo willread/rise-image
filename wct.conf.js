ae06df36e6f01adfd756bf3741fb9f723bb175df
Stuart Lees <stuartlees@gmail.com>
2019-05-09 10:31:11 -0400

    Fix security vulnerabilities

--- wct.conf.js
+++ wct.conf.js
@@ -1,9 +1,10 @@
 module.exports = {
   verbose: true,
+  npm: true,
+  moduleResolution: "node",
   plugins: {
     local: {
       browsers: [ "chrome" ]
     }
-  },
-  wctPackageName: "wct-browser-legacy"
+  }
 };
